54868d87ccb1efaa01098990a4fd6c285858dbc2
Keisuke Nakayama <me@yammer.jp>
2023-06-12 19:01:41 +0900

    add /help

--- web/main.py
+++ web/main.py
@@ -23,24 +23,28 @@ index = load_index_from_storage(storage_context)
 query_engine = index.as_query_engine()
 
 @app.route("/")
-def index():
+def routing_index():
     return render_template("main.html")
+
+@app.route("/help")
+def routing_help():
+    return render_template("help.html")
 
 @app.route("/limitcheck")
 @limiter.limit("5 per minute")
-def limitcheck():
+def routing_limitcheck():
     return "ok"
 
 @app.route("/api/search", methods=["POST"])
 @limiter.limit("100 per day")
-def apiSearch():
+def routing_apiSearch():
     req = request.get_json()
     query = req['query']
     if (query == ""):
         return {"error": "need query parameter"}, 400
 
     response = openai.Completion.create(
-        prompt= "Translate to English: " + req['query']
+        prompt= "Translate to English: " + req['query'],
         model="text-davinci-003"
     )
     queryEnglish = response["choices"][0]["text"].strip().strip("Title").strip("タイトル").strip(":").strip()
